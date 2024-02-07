1b605011f7ae3eb37260c681d9f76fccfa412359
oluobamzy <oluobamzy@gmail.com>
2024-02-06 20:09:58 -0500

    setting layouts

---
+++ boost/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from "../ui/dashboard/navbar/navbar";
+import Sidebar from "../ui/dashboard/sidebar/sidebar";
+
+ const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  )
+}
+export default Layout
---
+++ boost/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to Boost!</h1>
+     
+      
+    </main>
+  );
+}
